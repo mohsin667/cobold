fdd10e5c771e16d5d4f99322c6f67aa146874774
Mohsin Ansari <ansarimohsin667@gmail.com>
2020-10-19 11:50:14 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const trigger = document.querySelector("#mobTrigger");
+    const nav = document.querySelector("nav");
+    const slideBack = document.querySelector(".logo .slide-arrow")
+    trigger.addEventListener("click",e=>{
+        nav.classList.add("flaot")
+    })
+    slideBack.addEventListener("click",e=>{
+        nav.classList.remove("flaot")
+        
+    })
+
+    // banner slider
+    $('.banner-slider').on('init', function(event, slick){
+        $(".inner-slides").eq(0).addClass("animate");
+    });
+    $(".banner-slider").slick({
+        dots:true,
+        fade:true
+    })
+    $('.banner-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $(".inner-slides").removeClass("animate");
+        $("[data-slick-index="+nextSlide+"]").addClass("animate");
+      });
+
+});
+
+
